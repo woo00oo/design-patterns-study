917d108ad7798361f12457f9d96c34482665967c
woo00oo <96woo94@naver.com>
2022-03-09 17:24:19 +0900

    팩토리 메소드 패턴 - 패턴 소개

---
+++ src/main/java/factorymethod/before/Client.java
@@ -0,0 +1,19 @@
+package factorymethod.before;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Ship whiteship = ShipFactory.orderShip("Whiteship", "keesun@mail.com");
+        System.out.println(whiteship);
+
+        Ship blackship = ShipFactory.orderShip("Blackship", "keesun@mail.com");
+        System.out.println(blackship);
+    }
+
+}
+/*
+
+    해당 애플리케이션은 OCP(개방 폐쇄 원칙)에 만족하지 않음.
+    새로운 기능이 추가될 때 마다, ShipFactory 클래스에 변경이 생김.
+
+ */
