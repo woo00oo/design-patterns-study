9cd52489f7e2f231e73b0007a73e0fd7b966ef9d
woo00oo <96woo94@naver.com>
2022-03-12 18:38:49 +0900

    컴포짓 패턴 - 패턴 소개

---
+++ src/main/java/_02_structural_patterns/_08_composite/bofore/Item.java
@@ -0,0 +1,17 @@
+package _02_structural_patterns._08_composite.bofore;
+
+public class Item {
+
+    private String name;
+
+    private int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getPrice() {
+        return this.price;
+    }
+}
