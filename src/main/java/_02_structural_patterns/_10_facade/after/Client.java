26d2ab35a03e77492a42eae00bf4c99538f6263e
woo00oo <96woo94@naver.com>
2022-03-13 18:06:03 +0900

    퍼사드 패턴 - 패턴 적용

---
+++ src/main/java/_02_structural_patterns/_10_facade/after/Client.java
@@ -0,0 +1,18 @@
+package _02_structural_patterns._10_facade.after;
+
+public class Client {
+
+    public static void main(String[] args) {
+        EmailSettings emailSettings = new EmailSettings();
+        emailSettings.setHost("127.0.0.1");
+
+        EmailSender emailSender = new EmailSender(emailSettings);
+        EmailMessage emailMessage = new EmailMessage();
+        emailMessage.setFrom("keesun");
+        emailMessage.setTo("whiteship");
+        emailMessage.setSubject("오징어게임");
+        emailMessage.setText("밖은 더 지옥이더라고..");
+        emailSender.sendEmail(emailMessage);
+
+    }
+}
---
+++ src/main/java/_02_structural_patterns/_10_facade/after/EmailSettings.java
@@ -0,0 +1,14 @@
+package _02_structural_patterns._10_facade.after;
+
+public class EmailSettings {
+
+    private String host;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+}
