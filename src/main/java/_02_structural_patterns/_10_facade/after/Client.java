39c01c4c35efa21f850aaae2d880ecfa538628ba
woo00oo <96woo94@naver.com>
2022-03-13 18:20:15 +0900

    퍼사드 패턴 - 패턴 개념 정리

--- src/main/java/_02_structural_patterns/_10_facade/after/Client.java
+++ src/main/java/_02_structural_patterns/_10_facade/after/Client.java
@@ -12,7 +12,20 @@ public class Client {
         emailMessage.setTo("whiteship");
         emailMessage.setSubject("오징어게임");
         emailMessage.setText("밖은 더 지옥이더라고..");
+
         emailSender.sendEmail(emailMessage);
 
     }
 }
+
+/*
+    퍼사드 패턴
+    > 복잡한 서브 시스템 의존성을 최소화하는 방법.
+
+    장점
+    - 서브 시스템에 대한 의존성을 한 곳으로 모을 수 있다.
+
+    단점
+    - 퍼사드 클래스가 서브 시스템에 대한 모든 의존성을 가지게 된다.
+
+ */
