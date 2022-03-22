951e168a0894881578d8fb8c7f4ec0cfc572a2fd
woo00oo <96woo94@naver.com>
2022-03-22 23:05:02 +0900

    이터레이터 패턴 - 패턴 적용

---
+++ src/main/java/_03_behavioral_patterns/_16_iterator/after/Board.java
@@ -0,0 +1,25 @@
+package _03_behavioral_patterns._16_iterator.after;
+
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Board {
+
+    List<Post> posts = new ArrayList<>();
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void addPost(String content) {
+        this.posts.add(new Post(content));
+    }
+
+    public Iterator<Post> getRecentPostIterator() {
+        return new RecentPostIterator(this.posts);
+    }
+
+
+}
