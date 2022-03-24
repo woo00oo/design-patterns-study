ce981da02e215b56ff6addf81e18c44b0e2cfa37
woo00oo <96woo94@naver.com>
2022-03-24 23:02:06 +0900

    메멘토 패턴 - 패턴 적용

---
+++ src/main/java/_03_behavioral_patterns/_18_memento/after/GameSave.java
@@ -0,0 +1,21 @@
+package _03_behavioral_patterns._18_memento.after;
+
+public final class GameSave {
+
+    private final int blueTeamScore;
+
+    private final int redTeamScore;
+
+    public GameSave(int blueTeamScore, int redTeamScore) {
+        this.blueTeamScore = blueTeamScore;
+        this.redTeamScore = redTeamScore;
+    }
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+}
