025afdf11d01ce5d08e4e63b5742d54694a9d93c
woo00oo <96woo94@naver.com>
2022-03-24 22:54:22 +0900

    메멘토 패턴 - 패턴 소개

---
+++ src/main/java/_03_behavioral_patterns/_18_memento/before/Game.java
@@ -0,0 +1,24 @@
+package _03_behavioral_patterns._18_memento.before;
+
+public class Game {
+
+    private int redTeamScore;
+
+    private int blueTeamScore;
+
+    public int getRedTeamScore() {
+        return redTeamScore;
+    }
+
+    public void setRedTeamScore(int redTeamScore) {
+        this.redTeamScore = redTeamScore;
+    }
+
+    public int getBlueTeamScore() {
+        return blueTeamScore;
+    }
+
+    public void setBlueTeamScore(int blueTeamScore) {
+        this.blueTeamScore = blueTeamScore;
+    }
+}
