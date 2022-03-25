08464255c6605ba7a78d1d88709ad52fac58d8ef
woo00oo <96woo94@naver.com>
2022-03-25 23:40:53 +0900

    옵저버 패턴 - 패턴 적용

---
+++ src/main/java/_03_behavioral_patterns/_19_observer/after/Client.java
@@ -0,0 +1,18 @@
+package _03_behavioral_patterns._19_observer.after;
+
+public class Client {
+
+    public static void main(String[] args) {
+        ChatServer chatServer = new ChatServer();
+        User user1 = new User("keesun");
+        User user2 = new User("whiteship");
+
+        chatServer.register("오징어게임", user1);
+        chatServer.register("오징어게임", user2);
+
+        chatServer.register("디자인패턴", user1);
+
+        chatServer.sendMessage(user1, "오징어게임", "아..이름이 기억났어.. 일남이야...오일남");
+        chatServer.sendMessage(user2, "디자인패턴", "옵저버 패턴으로 만든 채팅");
+    }
+}
---
+++ src/main/java/_03_behavioral_patterns/_19_observer/after/Subscriber.java
@@ -0,0 +1,6 @@
+package _03_behavioral_patterns._19_observer.after;
+
+public interface Subscriber {
+
+    void handleMessage(String message);
+}
